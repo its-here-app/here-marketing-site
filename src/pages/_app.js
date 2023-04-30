2882db96f693a0da195e888ce4eb187997c428cd
Remy Hunter <remyhunter256@gmail.com>
2023-04-30 15:07:54 -0400

    add arrow cursor animation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,32 +1,5 @@
-import { useRouter } from "next/router";
-import { useRef } from "react";
-import { LocomotiveScrollProvider } from "react-locomotive-scroll";
-
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  const { asPath } = useRouter();
-  const containerRef = useRef(null);
-  return (
-    <LocomotiveScrollProvider
-      options={{
-        smooth: true,
-        // ... all available Locomotive Scroll instance options
-      }}
-      watch={
-        [
-          //..all the dependencies you want to watch to update the scroll.
-          //  Basicaly, you would want to watch page/location changes
-          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-        ]
-      }
-      location={asPath}
-      onLocationChange={scroll => scroll.scrollTo(0, { duration: 0, disableLerp: true })}
-      containerRef={containerRef}
-    >
-      <div data-scroll-container ref={containerRef}>
-        <Component {...pageProps} />
-      </div>
-    </LocomotiveScrollProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,42 +3,88 @@ import sticker1 from "/public/stickers/1.svg";
 import sticker2 from "/public/stickers/2.svg";
 import sticker3 from "/public/stickers/3.svg";
 import sticker4 from "/public/stickers/4.svg";
-import { Inter } from "next/font/google";
-import { useLocomotiveScroll, LocomotiveScrollProvider } from "react-locomotive-scroll";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useEffect, useState, useRef } from "react";
+import { gsap } from "gsap";
+import Cursor from "../components/Cursor.js";
 
 export default function Home() {
+  useEffect(() => {});
+
+  const cursorCircle = useRef(null);
+
+  // let cursor = document.getElementsByClassName("circle")[0];
+
+  const [MousePosition, setMousePosition] = useState({
+    left: 0,
+    top: 0,
+  });
+
+  const [hovering, setHovering] = useState(false)
+  // const [trailSize, setTrailSize] = useState(10)
+
+
+  useEffect(() => {
+    gsap.to(cursorCircle.current, {
+      duration: 0.3,
+ 
+      left: MousePosition.left - 12,
+      top: MousePosition.top - 12,
+    });
+  }, [MousePosition]);
+
+  useEffect(() => {
+    // trailSize = 300;
+    let foo = cursorCircle.current;
+    console.log(foo)
+    // foo.classList.add('grow')
+    hovering ? foo.classList.add('grow') : foo.classList.remove('grow')
+    // gsap.to(cursorCircle.current, {
+    //   duration: 0.3,
+    //   onRepeat: () =>
+    //   width: trailSize,
+    //   height: trailSize,
+    //   left: MousePosition.left - 12,
+    //   top: MousePosition.top - 12,
+    // })
+  }, [hovering])
+  const handleMouseMove = (e) => {
+    setMousePosition({ left: e.clientX, top: e.clientY });
+  };
 
   return (
-    <div data-scroll-section className="flex flex-col w-full h-full bg-black">
+    <div
+      onMouseMove={handleMouseMove}
+      className="cursor-none flex m-0 p-0 flex-col w-full h-auto bg-black"
+    >
+      {/* <Cursor MousePosition={MousePosition} /> */}
+
+      <div ref={cursorCircle} className="circle"></div>
+
       {/* main */}
-      <section className="h-[800px] flex items-center justify-center w-full">
+      <section className="h-[100vh] flex items-center justify-center w-full">
         <div className="relative w-[50vw] max-w-[900px] aspect-square ">
           <div
             id="bg"
             className="absolute w-[30%] h-[30%] top-[0px] left-[45%]"
           >
-            <Image fill src={sticker3} />
+            <Image fill alt="test" src={sticker3} />
           </div>
-          <div className="absolute w-[50%] h-[50%] top-[25%] left-[45%]">
-            <Image fill className="spin" src={sticker4} />
-          </div>
+          <a href="https://instagram.com/itshere.app" onMouseOver={() => setHovering(true)} onMouseLeave={() => setHovering(false)} className="cursor-none absolute w-[50%] h-[50%] top-[25%] left-[45%]">
+            <Image fill alt="test" className="spin" src={sticker4} />
+          </a>
           <div className="absolute w-[60%] h-[60%] bottom-[0px] left-[0px]">
-            <Image fill src={sticker1} />
+            <Image fill alt="test" src={sticker1} />
           </div>
 
           <div className="absolute w-[50%] h-[50%] top-[10%] left-[0px]">
-            <Image fill src={sticker2} />
+            <Image fill alt="test" src={sticker2} />
           </div>
         </div>
       </section>
       {/* /main */}
       {/* footer */}
-      
-      <section className="h-[800px] w-full bg-[#252525]">
-        howdy
-      </section>
+
+      {/* <section className="h-[50vh] w-full bg-[#252525]">howdy</section> */}
       {/* /footer */}
     </div>
   );
