b06c69c2cbbf0beb1d19148efaa35fa0f57bfe46
Remy Hunter <remyhunter256@gmail.com>
2023-04-30 12:40:16 -0400

    add locomotive scroll

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,32 @@
-import '@/styles/globals.css'
+import { useRouter } from "next/router";
+import { useRef } from "react";
+import { LocomotiveScrollProvider as RLSProvider } from "react-locomotive-scroll";
+
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { asPath } = useRouter();
+  const containerRef = useRef(null);
+  return (
+    <RLSProvider
+      options={{
+        smooth: true,
+        // ... all available Locomotive Scroll instance options
+      }}
+      watch={
+        [
+          //..all the dependencies you want to watch to update the scroll.
+          //  Basicaly, you would want to watch page/location changes
+          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+        ]
+      }
+      // location={asPath}
+      // onLocationChange={scroll => scroll.scrollTo(0, { duration: 0, disableLerp: true })}
+      containerRef={containerRef}
+    >
+      <div data-scroll-container ref={containerRef}>
+        <Component {...pageProps} />
+      </div>
+    </RLSProvider>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,27 +4,30 @@ import sticker2 from '/public/stickers/2.svg';
 import sticker3 from '/public/stickers/3.svg';
 import sticker4 from '/public/stickers/4.svg';
 import { Inter } from 'next/font/google'
+import { useLocomotiveScroll } from 'react-locomotive-scroll';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
+  const { scroll } = useLocomotiveScroll();
   
   return (
-  <div  className="flex w-full h-[100vh] bg-black items-center justify-center">
-    <div class="relative w-[50vw] max-w-[900px] aspect-square ">
+    
+  <div data-scroll-section className="flex w-full h-[800vh] bg-black items-center justify-center">
+    <div className="relative w-[50vw] max-w-[900px] aspect-square ">
 
-      <div id="bg" class="absolute w-[30%] h-[30%] top-[0px] left-[45%]" >
+      <div data-scroll data-scroll-direction="vertical" data-scroll-speed="6" id="bg" className="absolute w-[30%] h-[30%] top-[0px] left-[45%]" >
         <Image fill src={sticker3}/>
       </div>
-      <div class="absolute w-[50%] h-[50%] top-[25%] left-[45%]" >
-        <Image fill class="spin" src={sticker4}/>
+      <div className="absolute w-[50%] h-[50%] top-[25%] left-[45%]" >
+        <Image fill className="spin" src={sticker4}/>
       </div>
-      <div class="absolute w-[60%] h-[60%] bottom-[0px] left-[0px]" >
+      <div className="absolute w-[60%] h-[60%] bottom-[0px] left-[0px]" >
         <Image fill src={sticker1}/>
       </div>
       
-      <div class="absolute w-[50%] h-[50%] top-[10%] left-[0px]" >
+      <div className="absolute w-[50%] h-[50%] top-[10%] left-[0px]" >
         <Image fill src={sticker2}/>
       </div>
     
