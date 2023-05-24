f24eec4f5f26df32f3cb0519c79d54051e4f288d
Remy Hunter <remyhunter256@gmail.com>
2023-05-24 16:35:41 -0400

    fix carousel dragging

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -12,7 +12,6 @@ const SampleListsCarousel = () => {
   const carousel = useRef(null);
 
   /* for dragging */
-  const [currentIndex, setCurrentIndex] = useState(0);
   const [currentPosition, setCurrentPosition] = useState(0);
   const [dragStartPoint, setDragStartPoint] = useState(0);
   const [isDragging, setIsDragging] = useState(false);
@@ -46,15 +45,12 @@ const SampleListsCarousel = () => {
 
   useEffect(() => {
     // TODO: allow for dynamic resizing
-
   }, []);
 
   useEffect(() => {
     setIncrement(carouselWidth / sampleLists.length);
   }, [carouselWidth]);
-  useEffect(() => {
-    // carousel.current.style.transform = `translate3D(0px, 0px, 0px)`;
-  }, [currentIndex]);
+
 
   const next = () => {
     let maths = (-sampleLists.length * increment) / 2;
@@ -62,56 +58,53 @@ const SampleListsCarousel = () => {
     if (position > maths) {
       setPosition((prevState) => prevState - increment);
     }
-    sampleListContainer.current.scrollIntoView({ behavior: "smooth", block: "start" });
+    // sampleListContainer.current.scrollIntoView({ behavior: "smooth", block: "start" });
   };
 
   const prev = () => {
     if (position < 0) setPosition((prevState) => prevState + increment);
   };
+  // handle drag start
+  // handle drag
+  // handle drag end
 
+  const handleDragStart = (event) => {
+    event.type === 'touchstart' ? setDragStartPoint(event.touches[0].clientX - currentPosition) : setDragStartPoint(event.clientX - currentPosition)
+    setIsDragging(true);
+  };
 
-  // const handleDragStart = (event) => {
-  //   event.type === 'touchstart' ? setDragStartPoint(event.touches[0].clientX) : setDragStartPoint(event.clientX)
-  //   setIsDragging(true);
-  // };
+  const handleDrag = (event) => {
+    if (!isDragging) return;
+    let currentDragPoint = event.type === "touchmove" ? event.touches[0].clientX : event.clientX
+    let diff = currentDragPoint - dragStartPoint;
+    carousel.current.style.transform = `translateX(${diff}px)`;
+    setCurrentPosition(diff)
+  };
 
-  // const handleDrag = (event) => {
-  //   if (!isDragging) return;
+  const handleDragEnd = () => {
+    if (!isDragging) return;
+    setIsDragging(false);
 
-  //   let currentDragPoint = event.type === "touchmove" ? event.touches[0].clientX : event.clientX
-  //   let diff = currentDragPoint - dragStartPoint;
-  //   console.log(diff);
-
-  //   carousel.current.style.transform = `translateX(${diff}px)`;
-  // };
-
-  // const handleDragEnd = () => {
-  //   if (!isDragging) return;
-
-  //   setIsDragging(false);
-
-  //   // const threshold = 50;
-
-  //   // if (diff > (-sampleLists.length * increment) / 2) {
-  //   //   setCurrentIndex(2);
-  //   //   console.log('new position', currentIndex)
-  //   // }
-  //   // setStartX(diff);
-  //   // carousel.current.style.transform = "translate3D(0, 0, 0)";
-  // };
+    // const threshold = 50;
+    // if (diff > (-sampleLists.length * increment) / 2) {
+    //   setCurrentIndex(2);
+    //   console.log('new position', currentIndex)
+    // }
+    // setStartX(diff);
+    // carousel.current.style.transform = "translate3D(0, 0, 0)";
+  };
 
   return (
     <section ref={sampleListContainer} className="mx-auto overflow-hidden">
       <div
         ref={carousel}
-        // onMouseDown={handleDragStart}
-        // onMouseMove={handleDrag}
-        // onMouseUp={handleDragEnd}
+        onMouseDown={handleDragStart}
+        onMouseMove={handleDrag}
+        onMouseUp={handleDragEnd}
         // onTouchStart={handleDragStart}
         // onTouchMove={handleDrag}
         // onTouchEnd={handleDragEnd}
-        className={`transition-transform duration-[800ms] ease-[cubic-bezier(.23,1,.32,1)] flex flex-row gap-[2vw] pt-[2rem] text-[5rem] mx-auto subtitle-text px-[5px]`}
-        data-start-x="20"
+        className={`transition-transform  duration-[800ms] ease-[cubic-bezier(.23,1,.32,1)] flex flex-row gap-[2vw] pt-[2rem] text-[5rem] mx-auto subtitle-text px-[5px]`}
         style={{
           // transform: `translate3d(${position}px, 0px, 0px)`,
           width: `${carouselWidth}vw`,
@@ -121,31 +114,25 @@ const SampleListsCarousel = () => {
       >
         {sampleLists.map((currentList, index) => {
           return (
-            <div key={index} data-hover-style="tile" data-fade-in-group="2">
+            <div key={index} data-hover-style="tile" data-fade-in-group="2" className="">
               <div className="w-[80vw] md:w-[40vw] lg:w-[30vw] col-span-1 mx-[5px] my-[5px] aspect-[1/1] overflow-hidden bg-white  rounded-[1rem] transition-all">
-                <Link className="cursor-none scale(110%)" href="./">
-                  <div
-                    style={{ backgroundImage: `url(${currentList.img})` }}
-                    className="bg-cover bg-center w-full h-full transition-all ease-in duration-[1200ms] items-center justify-center grid grid-cols-1 grid-rows-3"
-                  >
-                      <div className="row-span-1">
-
-                      </div>
-                      <div className="flex row-span-1 select-none  text-[--neon] flex-col justify-center items-center">
-                        <div className="text-[2rem] sm:text-[40%] font-[Crimson] font-italic leading-[100%] translate-y-[20%]">
-                          {currentList.title}
-                        </div>
-                        <div className="text-[2rem] sm:text-[40%] font-[Golos] font-bold leading-[100%]">
-                          {currentList.subtitle}
-                        </div>
-                      </div>
-                      <div className="text-[--neon] pb-[1rem] self-end text-[2rem] sm:text-[40%] row-span-1 flex justify-center items-center">
-                        {currentList.amount}*
-                        {/* placeholder */}
-                      </div>
-
+                <div
+                  style={{ backgroundImage: `url(${currentList.img})` }}
+                  className="cursor-none scale(110%) select-none bg-cover bg-gray-400 bg-center w-full h-full transition-all ease-in duration-[1200ms] items-center justify-center grid grid-cols-1 grid-rows-3"
+                >
+                  <div className="row-span-1"></div>
+                  <div className="flex row-span-1   text-[--neon] flex-col justify-center items-center">
+                    <div className="text-[2rem] sm:text-[40%] font-[Crimson] font-italic leading-[100%] translate-y-[20%]">
+                      {currentList.title}
+                    </div>
+                    <div className="text-[2rem] sm:text-[40%] font-[Golos] font-bold leading-[100%]">
+                      {currentList.subtitle}
+                    </div>
                   </div>
-                </Link>
+                  <div className="text-[--neon] pb-[1rem] self-end text-[2rem] sm:text-[40%] row-span-1 flex justify-center items-center">
+                    {currentList.amount}*{/* placeholder */}
+                  </div>
+                </div>
               </div>
             </div>
           );
