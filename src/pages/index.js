a1fa0bf66333b1e27f1e62fbe59ca0bc53bc14ff
Remy Hunter <remyhunter256@gmail.com>
2023-09-28 16:18:44 -0400

    remove body animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -67,24 +67,6 @@ export default function Home() {
     setCarouselData(featured);
   };
 
-  // const animateBodyFade = (context) => {
-  //   context.animate(
-  //     [
-  //       {
-  //         opacity: 0,
-  //       },
-  //       {
-  //         opacity: 1,
-  //       },
-  //     ],
-  //     {
-  //       duration: 500,
-  //       easing: "ease-in-out",
-  //       fill: "forwards",
-  //     }
-  //   );
-  // };
-
   const flipperAnimationList = [
     {
       title: "New York City",
