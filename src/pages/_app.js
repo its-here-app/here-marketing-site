f56d977b50af691317ca816469d4be5acb59960c
Remy Hunter <remyhunter256@gmail.com>
2023-09-28 15:56:41 -0400

    add framer motion

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
 import "@/styles/globals.css";
-
+import { motion } from "framer-motion";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <motion.div
+      key={Component}
+      initial="pageInitial"
+      animate="pageAnimate"
+      transition={{ duration: 1.3, ease: [0.23, 1, 0.32,1]}}
+      variants={{
+        pageInitial: {
+          opacity: 0,
+          y: 30,
+        },
+        pageAnimate: {
+          opacity: 1,
+          y: 0,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  )
 }
